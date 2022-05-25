9e6d35f885e366242e204a82098453774988b6a9
Reish Evgeny <e1000057r@gmail.com>
2022-05-25 20:52:03 +0300

    home

--- tkinter_test_2.py
+++ tkinter_test_2.py
@@ -2,6 +2,17 @@ from tkinter import *
 from tkinter.ttk import Style
 
 
+def style_main(widget):
+    widget["background"] = "#ef9740"
+    if widget.widgetName == "button":
+        widget["activebackground"] = "#f3b271"
+        widget["bd"] = 0
+
+
+def style_sub(widget):
+    widget["background"] = "#f5c492"
+
+
 class App(Frame):
     actions = []
 
@@ -12,36 +23,35 @@ class App(Frame):
         self.initUI()
 
     def initUI(self):
-
         self.master.title("Action List")
-        self.style_sub(self)
-        self.style_sub(self.master)
+        style_sub(self)
+        style_sub(self.master)
 
         Style().configure("TButton", padding=(0, 5, 0, 5))
 
         lbl_welcome = Label(self, text="WELCOME")
         lbl_welcome.grid(row=0, column=0, columnspan=4, sticky=S, pady=5)
-        self.style_sub(lbl_welcome)
+        style_sub(lbl_welcome)
 
         self.txt_actions = Text(self, width=70, height=25)
         self.txt_actions.grid(row=1, column=0, columnspan=4, padx=5)
-        self.style_main(self.txt_actions)
+        style_main(self.txt_actions)
 
-        btn_insert = Button(self, text="INSERT", width=10, command=self.insert_actions)
-        btn_insert.grid(row=2, column=2, sticky="W", padx=5, pady=20)
-        self.style_main(btn_insert)
+        btn_insert = Button(self, text="INSERT", width=10, height=2, command=self.insert_actions)
+        btn_insert.grid(row=2, column=2, sticky="W", padx=5, pady=15)
+        style_main(btn_insert)
 
-        btn_show = Button(self, text="SHOW", width=10, command=self.show_actions)
-        btn_show.grid(row=2, column=2, sticky="E", padx=5, pady=20)
-        self.style_main(btn_show)
+        btn_show = Button(self, text="SHOW", width=10, height=2, command=self.show_actions)
+        btn_show.grid(row=2, column=2, sticky="E", padx=5, pady=15)
+        style_main(btn_show)
 
-        btn_exit = Button(self, text="EXIT", width=10, command=self.master.quit)
-        btn_exit.grid(row=2, column=3, sticky=E, padx=5, pady=20)
-        self.style_main(btn_exit)
+        btn_exit = Button(self, text="EXIT", width=10, height=2, command=self.master.quit)
+        btn_exit.grid(row=2, column=3, sticky=E, padx=5, pady=15)
+        style_main(btn_exit)
 
         self.lbl_actions = Label(self, text="")
         self.lbl_actions.grid(row=2, column=0, columnspan=3, sticky=W, padx=5, pady=5)
-        self.style_sub(self.lbl_actions)
+        style_sub(self.lbl_actions)
 
         self.pack()
 
@@ -59,7 +69,7 @@ class App(Frame):
 
     def show_actions(self):
         actions_view = Toplevel(self.master)
-        self.style_sub(actions_view)
+        style_sub(actions_view)
 
         resize_window(actions_view, 150, 250)
 
@@ -68,30 +78,21 @@ class App(Frame):
 
         lbl_act = Label(master=actions_view, text="ACTIONS")
         lbl_act.pack(side=TOP, padx=5, pady=5)
-        self.style_sub(lbl_act)
+        style_sub(lbl_act)
 
         listbox = Listbox(actions_view)
         for i in range(len(self.actions)):
             listbox.insert(i, self.actions[i])
         listbox.pack(side=TOP)
-        self.style_main(listbox)
+        style_main(listbox)
 
-        btn_exit = Button(master=actions_view, text="QUIT", command=actions_view.destroy)
-        btn_exit.pack(side=TOP, padx=5, pady=5)
-        self.style_main(btn_exit)
-
-    def style_main(self, widget):
-        widget["background"] = "#ef9740"
-        if widget.widgetName == "button":
-            widget["activebackground"] = "#f3b271"
-            widget["bd"] = 0
-
-    def style_sub(self, widget):
-        widget["background"] = "#f5c492"
+        btn_exit = Button(master=actions_view, text="QUIT", width=5, height=2, command=actions_view.destroy)
+        btn_exit.pack(side=TOP, padx=4, pady=8)
+        style_main(btn_exit)
 
 
-def resize_window(root, wight, heigth):
-    size = [wight, heigth]
+def resize_window(root, wight, height):
+    size = [wight, height]
     w = root.winfo_screenwidth()
     h = root.winfo_screenheight()
     w = w // 2
