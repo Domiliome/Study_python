5aaa990c6eb476d0f750c68668e1b1699e817397
Reish Evgeny <e1000057r@gmail.com>
2022-06-10 15:49:16 +0300

    home

---
+++ ArgsKwargs.py
@@ -0,0 +1,21 @@
+"""
+О том как работает *args и *kwargs
+Такая запись придаёт гибкость программе
+"""
+
+
+# создаётся кортеж args с аргументами
+def print_args(*args):
+    args = list(args)
+    for i in range(len(args)):
+        print(f"{i + 1}-й аргумент: {args[i]}")
+
+
+# создаётся словарь kwargs c названиями и значениями аргументов
+def print_kwargs(**kwargs):
+    for key, value in kwargs.items():
+        print(f"Значение для {key} является {value}")
+
+
+print_args(0, 2, 3)
+print_kwargs(my_name="Sammy", your_name="Casey")
--- Patterns/Observer.py
+++ Patterns/Observer.py
@@ -23,12 +23,10 @@ class Subscriber:
 
 publisher = Publisher()
 
-# Create observers that will receive notifications
 bucky = Subscriber('Bucky')
 sally = Subscriber('Sally')
 publisher.register(bucky)
 publisher.register(sally)
 
-# Notifications are sent to every observer (subscriber)
 publisher.send_notifications('We updated the website')
 publisher.send_notifications('Make sure to add a profile picture')
