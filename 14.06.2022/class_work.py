fbe166727dedb66162ed38b7b546d593e89cf0ab
Domiliome <80268574+Domiliome@users.noreply.github.com>
2022-06-14 18:27:40 +0300

    class

--- 14.06.2022/class_work.py
+++ 14.06.2022/class_work.py
@@ -1,4 +1,9 @@
 str = "ASD"
 
-if str.isupper():
-    print(str)
+if str.isupper():    # Проверка на регистр
+    pass
+
+str = "asd"
+str.strip()             # Удаление пробелов в начале и конце строки
+str.capitalize()        # Переводит первую букву в верхний регистр
+
---
+++ 14.06.2022/class_work_1.py
@@ -0,0 +1,68 @@
+from datetime import datetime
+from tkinter import *
+
+user = [{"name": "user", "message": "Hello", "datetime": "841241242"}]
+
+
+def resize_window(window, wight, height):
+    size = [wight, height]
+    w = window.winfo_screenwidth()
+    h = window.winfo_screenheight()
+    w = w // 2
+    h = h // 2
+    w = w - (size[0] // 2)
+    h = h - (size[1] // 2)
+    window.geometry('{}x{}+{}+{}'.format(size[0], size[1], w, h))
+
+
+def welcome(name):
+    if btn_login["text"] == "Войти":
+        ent_login.configure(state=DISABLED)
+        btn_login["text"] = "Выйти"
+        name = name.capitalize()
+        user.append(name)
+    else:
+        ent_login.configure(state=NORMAL)
+        ent_login.delete(0, END)
+        btn_login["text"] = "Войти"
+        user.clear()
+
+def send(message):
+    user.append(dict({"name": ent_login.get(), "message": ent_send, "datetime": datetime.now().strftime("%H:%M:%S ")}))
+
+
+
+
+root = Tk()
+resize_window(root, 600, 600)
+
+root.grid_columnconfigure(0, weight=1)
+root.grid_columnconfigure(1, weight=1)
+root.grid_columnconfigure(2, weight=1)
+root.grid_columnconfigure(3, weight=1)
+
+root.grid_rowconfigure(0, weight=1)
+root.grid_rowconfigure(1, weight=2)
+root.grid_rowconfigure(2, weight=3)
+
+lbl_send = Label(root, text="Сообщение: ")
+lbl_send.grid(row=0, column=0)
+ent_send = Entry(root)
+ent_send.grid(row=0, column=1)
+
+btn_send = Button(root, text="отправить", width=20, height=5, command=lambda: send(ent_send.get()))
+btn_send.grid(row=1, column=0, columnspan=2)
+
+lbl_login = Label(root, text="Имя: ")
+lbl_login.grid(row=0, column=2)
+
+ent_login = Entry(root)
+ent_login.grid(row=0, column=3)
+
+btn_login = Button(root, text="Войти", width=20, height=5, command=lambda: welcome(ent_login.get()))
+btn_login.grid(row=1, column=2, columnspan=2)
+
+txt_chat = Text(root)
+txt_chat.grid(row=2, column=0, columnspan=4)
+
+root.mainloop()
