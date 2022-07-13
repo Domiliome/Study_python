1c4628c50f53fe97bb63c07b60772b246bbf6195
eugene <E1000057R@gmail.com>
2022-07-13 15:09:55 +0300

    home

--- server/soket_server.py
+++ server/soket_server.py
@@ -2,7 +2,7 @@ import socket
 
 server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_sock.bind(("localhost", 2048))
-server_sock.listen(5)
+server_sock.listen(1)   # количество клиентов
 while True:
     sock, addr = server_sock.accept()
     while True:
