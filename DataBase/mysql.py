489d189e60f89da1352482e23d00a687a22645b7
eugene <E1000057R@gmail.com>
2022-07-29 14:20:34 +0300

    home

---
+++ DataBase/mysql.py
@@ -0,0 +1,137 @@
+from tkinter import *
+from options import *
+
+import MySQLdb
+
+
+class Base:
+    def __init__(self, name_db):
+        self.name_db = name_db
+        self.__connection = None
+
+    def set_status_connection(self, status):
+        if status == "on":
+            try:
+                self.__connection = MySQLdb.connect(host="localhost",
+                                                    user="root",
+                                                    passwd="",
+                                                    db=f"{self.name_db}")
+            except MySQLdb.OperationalError:
+                print("Не найдена база данных")
+        elif status == "off":
+            try:
+                self.__connection.close()
+            except AttributeError:
+                print("Соединение не подключено")
+
+    def get_connection(self):
+        return self.__connection
+
+    def get_data_query(self, tabel, atributes, sort=""):
+        self.set_status_connection("on")
+        connection = self.get_connection()
+        cursor = connection.cursor()
+        cursor.execute(f"SELECT {','.join(atributes)} FROM {tabel} {sort}")
+        data = cursor.fetchall()
+        self.set_status_connection("off")
+        return data
+
+
+class TabelEditor(Tk):
+    def __init__(self):
+        super().__init__()
+        resize_window(self, 400, 400)
+
+        self.records = []
+
+        self.lbl_age = None
+        self.lbl_breed = None
+        self.lbl_name = None
+
+        self.create_space()
+        self.update_tabel()
+
+    def update_tabel(self):
+
+        db = Base("test")
+        data = db.get_data_query("horses", ("name", "breed", "age"))
+        print(data)
+
+    def create_space(self):
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_columnconfigure(1, weight=1)
+        self.grid_columnconfigure(2, weight=1)
+        self.grid_rowconfigure(0, weight=1)
+
+        self.lbl_name = Label(text="Имя")
+        self.lbl_name.grid(row=0, column=0)
+
+        self.lbl_breed = Label(text="Порода")
+        self.lbl_breed.grid(row=0, column=1)
+
+        self.lbl_age = Label(text="Возраст")
+        self.lbl_age.grid(row=0, column=2)
+
+
+class Auth(Tk):
+    def __init__(self):
+        super().__init__()
+        resize_window(self, 200, 150)
+
+        self.lbl_check = None
+        self.ent_login = None
+        self.ent_pass = None
+        self.btn_entry = None
+
+        self.create_space()
+
+    def create_space(self):
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_columnconfigure(1, weight=5)
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_rowconfigure(1, weight=1)
+        self.grid_rowconfigure(2, weight=1)
+
+        lbl_login = Label(self, text="Login")
+        lbl_login.grid(row=0, column=0)
+
+        lbl_pass = Label(self, text="Password")
+        lbl_pass.grid(row=1, column=0)
+
+        self.lbl_check = Label(self, text="")
+        self.lbl_check.grid(row=2, column=1)
+
+        self.ent_login = Entry(self)
+        self.ent_login.grid(row=0, column=1)
+
+        self.ent_pass = Entry(self)
+        self.ent_pass.grid(row=1, column=1)
+
+        self.btn_entry = Button(self, text="Войти", command=self.editor)
+        self.btn_entry.grid(row=2, column=0, )
+
+    def editor(self):
+        if self.check_data():
+            self.destroy()
+            tab_edit = TabelEditor()
+            tab_edit.mainloop()
+        else:
+            self.lbl_check["text"] = "Неверный логин|пароль"
+
+    def check_data(self):
+        user = self.get_user_data()
+        db = Base("test")
+        data = db.get_data_query("users", ("login", "password"))
+        if user in data:
+            return True
+        else:
+            return False
+
+    def get_user_data(self):
+        user_login = self.ent_login.get()
+        user_pass = self.ent_pass.get()
+        return user_login, user_pass
+
+
+app = Auth()
+app.mainloop()
