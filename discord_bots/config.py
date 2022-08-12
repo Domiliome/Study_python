24389c59008063a726c97d051bdfed8e0386a147
eugene <E1000057R@gmail.com>
2022-08-12 19:22:11 +0300

    home

---
+++ discord_bots/config.py
@@ -0,0 +1,6 @@
+settings = {
+    'token': 'MTAwNzYzMjg0MTEzMzI2NDg5Ng.GLSGBp.I-nws-91jVfm8IyiCnN-_aWmrYFdVfUiKgdZR0',
+    'bot': 'FirstBot',
+    'id': 1007632841133264896,
+    'prefix': '$'
+}
---
+++ discord_bots/example_bot.py
@@ -0,0 +1,20 @@
+from config import settings
+import discord
+
+
+intents = discord.Intents.default()
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run(settings["token"])
